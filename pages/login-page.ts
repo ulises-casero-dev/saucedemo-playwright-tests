916a2472386103014adcef19e06339f1f52c2be2
Ulises Casero <ulises.git@gmail.com>
2026-08-13 15:35:56 -0300

    test: add login test suite and refactor to Page Object Model

---
+++ pages/login-page.ts
@@ -0,0 +1,36 @@
+import { expect, type Locator, type Page } from "@playwright/test";
+
+export class LoginPage {
+    // Variables
+    readonly page:Page;
+    readonly loginButton:Locator;
+    readonly URL = 'https://www.saucedemo.com/';
+
+
+    //Constructor
+    constructor (page:Page) {
+        this.page = page;
+        this.loginButton = page.getByRole('button', {name: 'Login'});
+    }
+
+    // Methods
+    async goto(){
+        await this.page.goto(this.URL);
+    }
+    
+    async clickLoginButton(){
+        await this.loginButton.click();
+    }
+
+    async fillLoginForm(user:string, pass:string){
+        await this.page.fill('#user-name', user);
+        await this.page.fill('#password', pass);
+    }
+
+    async expectErrorText(errorText: string) {
+        await expect(this.page.locator('[data-test="error"]')).toContainText(errorText);
+    }
+
+}
+
+export default LoginPage;
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,47 +1,95 @@
 import { test, expect, type Page } from '@playwright/test';
+import { LoginPage } from '../pages/login-page';
 
 test.describe('Login', () => {
+    let loginPage: LoginPage;
+
     test.beforeEach(async ({page}) => {
-        await page.goto("https://www.saucedemo.com/");
+        loginPage = new LoginPage(page);
+        await loginPage.goto();
     })
     
-    test('Login with wrong credentials', async ({page}) => {
+    test.only('Login with wrong credentials', async ({page}) => {
         //Arrange
         const user = "wrong_user";
-        const passwprd = "wrong_pass";
+        const password = "wrong_pass";
 
         //Act
-        await page.fill("#user-name", user);
-        await page.fill("#password", passwprd);
-        await page.getByRole('button', {name: 'Login'}).click();
+        await loginPage.fillLoginForm(user,password);
+        await loginPage.clickLoginButton();
 
         //Assert
-        //await expect(page.locator('[data-test="error"]')).toBeVisible();
-        const text = 'Epic sadface: Username and password do not match any user in this service';
-        await expect(page.locator('[data-test="error"]')).toContainText(text);
+        const errorText = 'Epic sadface: Username and password do not match any user in this service';
+        await loginPage.expectErrorText(errorText);
     })
 
     test('Login with correct credentials', async ({page}) =>{
+        //Arrange
         const user = 'standard_user';
         const password = 'secret_sauce';
 
-        await page.fill('#user-name', user);
-        await page.fill('#password', password);
-        await page.getByRole('button', {name: 'Login'}).click();
+        //Act
+        await loginPage.fillLoginForm(user,password);
+        await loginPage.clickLoginButton();
 
+        //Assert
         await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
     })
     
     test('Login with locked user', async ({page}) => {
+        //Arrange
         const user = 'locked_out_user';
         const password = 'secret_sauce';
 
-        await page.fill('#user-name', user);
-        await page.fill('#password', password);
-        await page.getByRole('button', {name: 'Login'}).click();
+        //Act
+        await loginPage.fillLoginForm(user,password);
+        await loginPage.clickLoginButton();
 
-        const lockedUserText = 'Epic sadface: Sorry, this user has been locked out.';
-        await expect(page.locator('[data-test="error"]')).toContainText(lockedUserText);
+        //Assert
+        const errorText = 'Epic sadface: Sorry, this user has been locked out.';
+        await loginPage.expectErrorText(errorText);
     })
 
+
+    test('Login with empty credentials', async ({page}) => {
+        //Arrange
+        const user = '';
+        const password = '';
+
+        //Act
+        await loginPage.fillLoginForm(user,password);
+        await loginPage.clickLoginButton();
+
+        //Assert
+        const errorText = 'Epic sadface: Username is required';
+        await loginPage.expectErrorText(errorText);
+    });
+
+    test('Login with empty password credential', async ({page}) => {
+        //Arrange
+        const user = 'standard_user';
+        const password = '';
+        
+        //Act
+        await loginPage.fillLoginForm(user,password);
+        await loginPage.clickLoginButton();
+
+        //Assert
+        const errorText = 'Epic sadface: Password is required';
+        await loginPage.expectErrorText(errorText);
+    });
+
+    test('Login with empty user credential', async ({page}) => {
+        //Arrange
+        const user = '';
+        const password = 'secret_sauce';
+
+        //Act
+        await loginPage.fillLoginForm(user,password);
+        await loginPage.clickLoginButton();
+
+        //Assert
+        const errorText = 'Epic sadface: Username is required';
+        await loginPage.expectErrorText(errorText);
+    })
 })
